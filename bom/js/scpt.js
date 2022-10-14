59efbed91a1306e7a9d61e9ecbb4fbe771c5a39d
iriskarina <iriskarina120120@gmail.com>
2022-10-14 02:01:16 -0300

    create bom

---
+++ bom/js/scpt.js
@@ -0,0 +1,26 @@
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  let myItem = input.value;
+  if (input.value != "") {
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = "X";
+    list.appendChild(listItem);
+    document.querySelector("#empty_value").textContent = "";
+    listBtn.onclick = function () {
+      list.removeChild(listItem);
+    };
+    input.focus();
+  } else {
+    document.querySelector("#empty_value").textContent =
+      "You have not added any chapter";
+  }
+};
---
+++ chamber/js/menu-ham.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    document.getElementById("nav-label").classList.toggle("open");
+    document.getElementById("burgger-btn").classList.toggle("open");
+}
+    const x = document.getElementById("burgger-btn");
+    x.onclick = toggleMenu;
+
+
+
